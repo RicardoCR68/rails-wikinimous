26583757e6ee734220b76a93b85d7445a3c06310
Ricardo Ribeiro <ricardoc6r8@gmail.com>
2019-05-21 15:33:35 -0300

    arcticles

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+puts 'Started'
+10.times do
+  new_article = Article.new(title: Faker::Book.title, content: Faker::Lorem.paragraph(4))
+  puts new_article.valid?
+  new_article.save
+
+  puts 'Saved a new Article'
+end
+
+puts "Ended with #{Article.count} new articles"
